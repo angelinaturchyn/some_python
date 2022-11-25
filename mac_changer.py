9d4c6651da1f5ee6b03027137d9f2e4b89aa454d
angelinaturchyn <75140309+angelinaturchyn@users.noreply.github.com>
2022-11-25 17:37:32 -0600

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env/python
 
 
+
 import subprocess
 
+interface = " eth0"
+new_address = "00:22:33:44:55:66"
+
+
 subprocess.call("ifconfig", shell=True)
+subprocess.call("ifconfig" + interface + " down ", shell=True)
+subprocess.call("ifconfig" + interface + " hw ether " + new_address, shell=True)
+subprocess.call("ifconfig" + interface + " up",  shell=True)
 
-subprocess.call("ifconfig eth0 down ", shell=True)
-
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55", shell=True)
-
-subprocess.call("ifconfig eth0 up",  shell=True)
+print("[+] Changing MAC address for " + interface + " to " + new_address)
